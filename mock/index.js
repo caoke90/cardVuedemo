ade2e58e448cdcf40634103a93067d8a11466c93
曹科 <caoke@staff.weibo.com>
2018-03-20 13:41:46 +0800

    21

---
+++ mock/index.js
@@ -0,0 +1,20 @@
+module.exports=[
+  {
+
+    //本地的url =》 本地的url
+    "/admin/mobile_page/edit":"/cardVue/index.html",
+    "/cardVue/demo.html?preview=1":"/cardVue/demo.html?preview=1",
+    "/all":"/cardVue/all.html",
+    "/cardVue/alldemo.html?preview=1":"/cardVue/alldemo.html?preview=1",
+  },{
+    "/pcadmin":"/cardVue/pcadmin.html",
+    //线上的url =》 本地的mock
+    "/subject/h5/getcardinfo":"/subject/h5/getcardinfo",
+    "/subject/h5/getpageinfo":"/subject/h5/getpageinfo",
+    "/subject/h5/callbackoperator":"/subject/h5/callbackoperator",
+    "/addfollow":"/subject/h5/ok",
+    "/admin/mobile_page/ajax_getpolllist":"/subject/h5/ajax_getpolllist",
+    "/getuserandtask":"/subject/h5/getuserandtask",
+    // "/subject/h5/savecard":"http://ting.weibo.com/subject/h5/savecard",
+  }
+]
